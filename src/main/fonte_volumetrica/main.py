7f61440140d1abe28cc377155016dcda23b19590
Alexandre Neves <alxfn86@gmail.com>
2024-07-22 16:59:48 -0300

    fix: deslocamento da fonte até 1.5x altura até a abertura

--- src/main/fonte_volumetrica/main.py
+++ src/main/fonte_volumetrica/main.py
@@ -18,7 +18,7 @@ def fonte_volumetrica(altura_abertura, raio_abertura, raio_cilindro, altura_cili
     arquivo = 'src/main/fonte_volumetrica/arquivos_gerados/DATA_VOL.txt'
     altura_ponto = 0 - INCREMENTO;
     if tipo_fonte:
-        while topo_fonte < altura_ate_a_abertura:   
+        while topo_fonte < (altura_ate_a_abertura * 1.5):   
             pts_no_angulo = 0
             topo_fonte = base_cilindro + 4.1
             for i in range(sorteios):
@@ -31,17 +31,18 @@ def fonte_volumetrica(altura_abertura, raio_abertura, raio_cilindro, altura_cili
                         
             salvar_arquivo(arquivo, topo_fonte, pts_no_angulo)
             base_cilindro += INCREMENTO
-    if (tipo_fonte == False):
-        pts_no_angulo = 0
-        altura_ponto += INCREMENTO
-        for i in range(sorteios):
-            print(f'Sorteios: {i+1}')
-            #x, y, z
-            ponto = np.array([0,0, altura_ponto])
-            if(sorteia_pontos(ponto, altura_ate_a_abertura, raio_abertura)):
-                pts_no_angulo += 1   
-        arquivo_pontual = f'src/main/fonte_volumetrica/arquivos_gerados/DATA_PONTUAL.txt'
-        salvar_arquivo(arquivo_pontual, altura_ponto, pts_no_angulo)
+    else:
+        while altura_ponto < (altura_ate_a_abertura * 1.5):
+            pts_no_angulo = 0
+            altura_ponto += INCREMENTO
+            for i in range(sorteios):
+                print(f'Sorteios: {i+1}')
+                #x, y, z
+                ponto = np.array([0,0, altura_ponto])
+                if(sorteia_pontos(ponto, altura_ate_a_abertura, raio_abertura)):
+                    pts_no_angulo += 1   
+            arquivo_pontual = f'src/main/fonte_volumetrica/arquivos_gerados/DATA_PONTUAL.txt'
+            salvar_arquivo(arquivo_pontual, altura_ponto, pts_no_angulo)
 
 
 
